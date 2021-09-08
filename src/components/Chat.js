5b9666945b3b60b67f6948094d6471e8ccd641f5
Nav 333 <naveen333nav@gmail.com>
2021-09-08 15:42:30 +0530

    pushing messages into firestore and populating chats section

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,176 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, IconButton, Menu, MenuItem } from '@material-ui/core'
+import {
+  AddPhotoAlternate,
+  ArrowBack,
+  EventNote,
+  MoreVert,
+} from '@material-ui/icons'
+import { useHistory, useParams } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+import Compressor from 'compressorjs'
+
+import ChatMessages from './ChatMessages'
+import ChatFooter from './ChatFooter'
+import MediaPreview from './MediaPreview'
+import useRoom from '../hooks/useRoom'
+import { createTimestamp, storage, db } from '../firebase'
 import './Chat.css'
 
 export default function Chat({ user, page }) {
+  const [image, setImage] = useState(null)
+  const [src, setSrc] = useState('')
+  const [input, setInput] = useState('')
+
+  const { roomId } = useParams()
+  const room = useRoom(roomId, user.uid)
+  const history = useHistory()
+
+  const onChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  const sendMessage = async (event) => {
+    event.preventDefault()
+    if (input.trim() || (input === '' && image)) {
+      setInput('')
+      if (image) {
+        closePreview()
+      }
+      const imageName = uuid()
+      const newMessage = image
+        ? {
+            name: user.displayName,
+            message: input,
+            uid: user.uid,
+            timestamp: createTimestamp(),
+            time: new Date().toUTCString(),
+            imageUrl: 'uploading',
+            imageName,
+          }
+        : {
+            name: user.displayName,
+            message: input,
+            uid: user.uid,
+            timestamp: createTimestamp(),
+            time: new Date().toUTCString(),
+          }
+
+      await db
+        .collection('users')
+        .doc(user.uid)
+        .collection('chats')
+        .doc(roomId)
+        .set({
+          name: room.name,
+          photoURL: room.photoURL || null,
+          timestamp: createTimestamp(),
+        })
+
+      const doc = await db
+        .collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .add(newMessage)
+
+      if (image) {
+        new Compressor(image, {
+          quality: 0.8,
+          maxWidth: 1920,
+          async success(result) {
+            setSrc('')
+            setImage(null)
+            await storage.child(imageName).put(result)
+            const url = await storage.child(imageName).getDownloadURL()
+
+            db.collection('rooms')
+              .doc(roomId)
+              .collection('messages')
+              .doc(doc.id)
+              .update({
+                imageUrl: url,
+              })
+          },
+        })
+      }
+    }
+  }
+
+  const showPreview = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      setImage(file)
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        setSrc(reader.result)
+      }
+    }
+  }
+
+  const closePreview = () => {
+    setSrc('')
+    setImage(null)
+  }
+
   return (
     <div className='chat'>
-      <h2>Chat area</h2>{' '}
+      <div style={{ height: page.height }} className='chat__background' />
+
+      <div className='chat__header'>
+        {page.isMobile && (
+          <IconButton onClick={() => history.goBack()}>
+            <ArrowBack />
+          </IconButton>
+        )}
+        <div className='avatar__container'>
+          <Avatar src={room?.photoURL} />
+        </div>
+        <div className='chat__header--info'>
+          <h3 style={{ width: page.isMobile && page.width - 165 }}>
+            {' '}
+            {room?.name}
+          </h3>
+        </div>
+
+        <div className='chat__header--right'>
+          <input
+            id='image'
+            style={{ display: 'none' }}
+            accept='image/*'
+            type='file'
+            onChange={showPreview}
+          />
+          <IconButton>
+            <label style={{ cursor: 'pointer', height: 24 }} htmlFor='image'>
+              <AddPhotoAlternate />
+            </label>
+          </IconButton>
+
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+          <Menu id='menu' keepMounted open={false}>
+            <MenuItem>DeleteRoom</MenuItem>
+          </Menu>
+        </div>
+      </div>
+      <div className='chat__body--container'>
+        <div className='chat__body' style={{ height: page.height - 68 }}>
+          <ChatMessages />
+        </div>
+      </div>
+
+      <MediaPreview src={src} closePreview={closePreview} />
+      <ChatFooter
+        input={input}
+        onChange={onChange}
+        sendMessage={sendMessage}
+        image={image}
+        user={user}
+        room={room}
+        roomId={roomId}
+      />
     </div>
   )
 }
--- src/components/ChatFooter.js
+++ src/components/ChatFooter.js
@@ -1,6 +1,101 @@
-import React from "react";
-import "./ChatFooter.css";
+import React, { useState } from 'react'
+import {
+  Send,
+  MicRounded,
+  CancelRounded,
+  CheckCircleRounded,
+} from '@material-ui/icons'
+import './ChatFooter.css'
 
-export default function ChatFooter() {
-  return <div className="chat__footer">ChatFooter</div>;
+export default function ChatFooter({
+  input,
+  onChange,
+  sendMessage,
+  image,
+  user,
+  room,
+  roomId,
+}) {
+  const [isRecording, setIsRecording] = useState(false)
+  const btnIcons = (
+    <>
+      <Send
+        style={{
+          width: 20,
+          height: 20,
+          color: 'white',
+        }}
+      />
+      <MicRounded
+        style={{
+          height: 24,
+          width: 24,
+          color: 'white',
+        }}
+      />
+    </>
+  )
+
+  const canRecord = navigator.mediaDevices.getUserMedia && window.MediaRecorder
+  return (
+    <div className='chat__footer'>
+      <form>
+        <input
+          type='text'
+          placeholder='type a message'
+          value={input}
+          onChange={!isRecording ? onChange : null}
+        />
+
+        {canRecord ? (
+          <button
+            type='submit'
+            className='send__btn'
+            onClick={
+              input.trim() || (input === '' && image)
+                ? sendMessage
+                : () => false
+            }
+          >
+            {btnIcons}
+          </button>
+        ) : (
+          <>
+            <label htmlFor='capture' className='send__btn'>
+              {btnIcons}
+            </label>
+            <input
+              type='file'
+              style={{ display: 'none' }}
+              id='capture'
+              accept='audio/*'
+              capture
+            />
+          </>
+        )}
+      </form>
+      {isRecording && (
+        <div className='record'>
+          <CancelRounded
+            style={{
+              width: 30,
+              height: 30,
+              color: '#f20519',
+            }}
+          />
+          <div>
+            <div className='record__redcircle'></div>
+            <div className='record__duration'></div>
+          </div>
+          <CheckCircleRounded
+            style={{
+              width: 30,
+              height: 30,
+              color: '#41bf49',
+            }}
+          />
+        </div>
+      )}
+    </div>
+  )
 }
